9fb89122b9add74b11df73eed2f3a1f5351852d2
Tovi Newman <deartovi@gmail.com>
2017-10-15 13:18:52 -0700

    improve filter algorithm

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var cars = [
   { make: 'Honda', image: 'images/honda-accord-2005.jpg', model: 'Accord', year: 2005, price: 7000},
   { make: 'Honda', image: 'images/honda-accord-2008.jpg', model: 'Accord', year: 2008, price: 11000 },
   { make: 'Toyota', image: 'images/toyota-camry-2009.jpg', model: 'Camry', year: 2009, price: 12500 },
-  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corrolla', year: 2016, price: 15000 },
+  { make: 'Toyota', image: 'images/toyota-corrolla-2016.jpg', model: 'Corrolla', year: 2013, price: 15000 },
   { make: 'Suzuki', image: 'images/suzuki-swift-2014.jpg', model: 'Swift', year: 2014, price: 9000 },
   { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 25000 },
   { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 26000 },
@@ -15,8 +15,6 @@ var shop = {
   },
   handleFilter: function(e) {
     e.preventDefault();
-    this.setFilter();
-    this.filterCars();
     this.renderCars();
   },
   setFilter: function() {
@@ -26,17 +24,17 @@ var shop = {
     }.bind(this));
   },
   filterCars: function() {
-    this.cars = this.cars.filter(function(car) {
+    this.filteredCars = this.cars.filter(function(car) {
       if (this.filter.make !== 'All' && this.filter.make !== car.make) return false;
       if (this.filter.model !== 'All' && this.filter.model !== car.model) return false;
-      if (this.filter.year !== 'All' && this.filter.year !== car.year) return false;
-      if (this.filter.price !== 'All' && this.filter.price !== car.price) return false;
+      if (this.filter.year !== 'All' && +this.filter.year !== car.year) return false;
+      if (this.filter.price !== 'All' && +this.filter.price !== car.price) return false;
       return true;
     }.bind(this));
   },
   renderCars: function() {
     this.$cars.children().remove();
-    this.cars.forEach(function(car) {
+    this.filteredCars.forEach(function(car) {
       $('#cars').append(this.loadTemplate(car, this.carTemplate));
     }.bind(this));
   },
@@ -44,26 +42,32 @@ var shop = {
     this.setFilter();
     this.filterCars();
     this.renderFilter();
-    $('#' + e.target.id).replaceWith(e.target);
+    this.setFilterValues();
   },
   renderFilter: function() {
-    $('#inputs').remove();
+    $('#selectors').children().remove();
     var unique = {};
-    unique.make = [];
-    unique.model = [];
-    unique.year = [];
-    unique.price = [];
-    this.cars.forEach(function(car) {
-      this.categories.forEach(function(category) {
+    this.categories.forEach(category => unique[category] = []);
+    this.filteredCars.forEach((car) => {
+      this.categories.forEach((category) => {
         if (!unique[category].includes(car[category])) unique[category].push(car[category]);
       });
-    }.bind(this));
+    });
     unique.price.sort((a, b) => a - b);
     unique.year.sort((a, b) => a - b);
-    this.categories.forEach(function(category) {
+    this.categories.forEach((category) => {
       var source = $('#' + category + '-template').html();
-      $('form').append(this.loadTemplate(unique, source));
-    }.bind(this));
+      $('#selectors').append(this.loadTemplate(unique, source));
+    });
+  },
+  setFilterValues: function() {
+    $('select').each((i, selector) => {
+      var $element = $('option[value="' + this.filter[selector.id] + '"]');
+
+      if ($element.val() !== 'All') {
+        $('option[value="' + this.filter[selector.id] + '"]')[0].selected = 'selected';
+      }
+    });
   },
   loadTemplate: function(context, source) {
     var template = Handlebars.compile(source);
@@ -77,6 +81,7 @@ var shop = {
 
     this.categories = ['make', 'model', 'year', 'price']
     this.cars = cars;
+    this.filteredCars = this.cars;
     this.renderCars();
     this.renderFilter();
     this.registerHandlers();
